50d5e8317e8573d60d7d7326141a704ff9739528
erwinwahyura <erwinwahyuramadhan@gmail.com>
2019-01-15 11:27:33 +0700

    add new field in table print barcode

--- app/components/main.js
+++ app/components/main.js
@@ -6,6 +6,7 @@ Vue.component('main-list-barcode', {
         <tr class="table-header">
           <th scope="col" class="col-for-no">No</th>
           <th scope="col" class="col-for-barcode">Barcode</th>
+          <th scope="col" class="col-for-qty">Qty Print</th>
           <th scope="col" class="col-for-sku">SKU</th>
         </tr>
       </thead>
@@ -13,6 +14,7 @@ Vue.component('main-list-barcode', {
         <tr class="table-body" v-for="(list, i) in listbarcodes" :key="i" scope="row">
           <th class="list-text for-no"> {{ i+1 }} </th>
           <th class="list-text for-barcode"> {{ list.barcode }} </th>
+          <th class="list-text for-qty"> {{ list.quantity_print }} </th>
           <th class="list-text for-sku"> {{ list.sku }} </th>
         </tr>
       </tbody>
