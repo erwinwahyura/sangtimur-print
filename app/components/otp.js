b6c1dd6a612cb2f59b8b3bb9d7098be489a52586
kevinhimawanhie <kevinhimawanhie@gmail.com>
2019-02-06 12:09:32 +0700

    update style parent, input code and button

--- app/components/otp.js
+++ app/components/otp.js
@@ -1,13 +1,13 @@
 Vue.component('otp-main-and-submit', {
   template: `
     <div class="otp-parent">
-      <div class="input-group mb-3">
+      <div class="input-group mb-3" style="width: 900px">
         <input
           v-model="otpCode"
           type="text"
           class="form-control"
           placeholder="Masukan kode print anda...">
-        <div class="input-group-append">
+        <div class="input-group-append" style="height: 38px">
           <button
             @click="emitOTP"
             class="btn btn-outline-secondary"
@@ -18,9 +18,10 @@ Vue.component('otp-main-and-submit', {
         </div>
       </div>
 
-      <div class="submit-print input-group mb-3">
-        <div class="custome-input-group-append">
+      <div class="submit-print input-group mb-3" style="width: calc(100% - 932px)">
+        <div class="custome-input-group-append" style="height: 38px; width: 100%">
           <button
+            style="height: 38px; width: 100%"
             @click="emitPRINT"
             class="btn btn-outline-secondary"
             type="button"
